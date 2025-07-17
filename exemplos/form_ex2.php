5bc75df6f89e39467891067928b7b6242393fef4
MatheusESouza21 <bmakgacha@gmail.com>
2025-07-16 21:39:32 -0300

    array funcao php

---
+++ exemplos/form_ex2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+</head>
+<body>
+    <form method="GET" action="back_form2.php">
+        <label> Nome </label>
+        <input name="Nome" type="text"/>
+        <label> Idade </label>
+        <input name="Idade" type="number"/>
+        <input type="submit" value="Enviar"/>  
+    </form>
+     
+</body>
+</html>
