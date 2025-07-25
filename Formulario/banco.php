e77133a7247cac74cfcc953b41fafabdffaac3b5
MatheusESouza21 <bmakgacha@gmail.com>
2025-07-24 22:13:38 -0300

    conexão banco

--- Formulario/banco.php
+++ Formulario/banco.php
@@ -1,21 +1,34 @@
 <?php
-    $bdServidor = "127.0.0.1"
-    $bdUsuario = 'root'
-    $bdSenha = ''
-    $bdBanco = 'matheuseduardo_db'
-    $conexao = mysql_connect($bdServidor, $bdUsuario, $bdSenha, $bdBanco);
-        if (mysqli_connect_errno()) {
-            echo "Problemas para conectar no banco. Verifique os dados!";
+    $bdServidor = '127.0.0.1';
+    $bdUsuario = 'root';
+    $bdSenha = '';  
+    $bdBanco = 'daniel_balera_ds';
+    $conexao = mysqli_connect($bdServidor, $bdUsuario, $bdSenha, $bdBanco);
+        if (mysqli_connect_errno()){
+            echo 'Problemas para conecar no banco.';
             die();
         }
 
-        function buscar_tarefas($conexao)
-        {
-            $sqlBusca = 'SELECT * FROM  tarefas';
+        function buscar_tarefas($conexao) {
+            $sqlBusca = 'SELECT * FROM tarefas';
             $resultado = mysqli_query($conexao, $sqlBusca);
             $tarefas = array();
-            while ($tarefa = mysqli_fetch_assoc($resultado)){
+            while ($tarefa = mysqli_fetch_assoc($resultado)) {
                 $tarefas[] = $tarefa;
             }
         return $tarefas;
         }
+
+        function gravar_tarefa($conexao, $tarefa){
+            $sqlGravar = "INSERT INTO tarefas (nome, descricao, prazo, prioridade, concluida) 
+            values
+            (
+                '{$tarefa['nome']}',
+                '{$tarefa['descricao']}',
+                '{$tarefa['prazo']}',
+                '{$tarefa['prioridade']}',
+                '{$tarefa['concluida']}'
+                )";
+            mysqli_query($conexao, $sqlGravar);
+        }
+    ?>
--- Formulario/tarefas.php
+++ Formulario/tarefas.php
@@ -1,42 +1,38 @@
 <?php
-    session_start();
-     include "banco.php"
-    
-    if (isset($_GET['nome']) && $_GET['nome'] != '') {
-        // Declarando uma array chamada 'tarefa'
-        $tarefa = array();
+session_start();
+include "banco.php";
 
-        $tarefa['nome'] = $_GET['nome'];
-    
-        if (isset($_GET['descricao'])) {
-                $tarefa['descricao'] = $_GET['descricao'];
-            } else {
-                $tarefa['descricao'] = '';
-        }
+if (isset($_GET['nome']) && $_GET['nome'] != '') {
+    $tarefa = array();
 
-        if (isset($_GET['prazo'])) {
-            $tarefa['prazo'] = $_GET['prazo'];
-        } else {
-            $tarefa['prazo'] = '';
-        }
+    $tarefa['nome'] = $_GET['nome'];
 
-        $tarefa['prioridade'] = $_GET['prioridade'];
-    
-        if (isset($_GET['concluida'])) {
-            $tarefa['concluida'] = $_GET['concluida'];
-        } else {
-            $tarefa['concluida'] = '';
-        }
-
-        $_SESSION['lista_tarefas'][] = $tarefa;
-    }
-
-        
-    if (array_key_exists('lista_tarefas', $_SESSION)) {
-        $lista_tarefas = $_SESSION['lista_tarefas'];
+    if (isset($_GET['descricao'])) {
+        $tarefa['descricao'] = $_GET['descricao'];
     } else {
-        $lista_tarefas = [];
+        $tarefa['descricao'] = '';
     }
 
-    include "template.php";
-?>
+    if (isset($_GET['prazo'])) {
+        $tarefa['prazo'] = $_GET['prazo'];
+    } else {
+        $tarefa['prazo'] = '';
+    }
+    $tarefa['prioridade'] = $_GET['prioridade'];
+
+    if (isset($_GET['concluida'])) {
+        $tarefa['concluida'] = $_GET['concluida'];
+    } else {
+        $tarefa['concluida'] = '';
+    }
+    $_SESSION['lista_tarefas'][] = $tarefa;
+}
+
+$lista_tarefas = buscar_tarefas($conexao);
+
+
+
+
+//session_destroy();
+include "template.php";
+    ?>
