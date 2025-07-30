0ed1ce2db4cf535f4ef043d3d74d767c2fa5711e
MatheusESouza21 <bmakgacha@gmail.com>
2025-07-30 19:33:10 -0300

    atualizar e deletar cliente

---
+++ conexão/PDO/deletarCliente.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Excluir Cliente</title>
+    <link rel="stylecheet href='style.css">
+</head>
+
+<body>
+    <h2>Excluir Cliente</h2>
+    <form action="processarExclusao.php" method="post">
+        <label for="id">ID do Cliente:</label>
+        <input type="number" id="id" name="id" required>
+
+        <button type="submit">Excluir Cliente</button>
+</body>
+
+</html>
